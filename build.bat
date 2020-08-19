b45bfeb95e71122342b6f732d9cd8c0fd624e3d6
NeGate <yasserarguelles2506@gmail.com>
2020-08-19 00:02:24 -0400

    Fixed build script

--- build.bat
+++ build.bat
@@ -1,11 +1,9 @@
 @echo off
-rem Setup your GNU Assembler to your PATH
-rem Setup your MSVC Linker to your PATH
+rem Make sure to link your GNU Assembler and MSVC Linker to PATH
+rem Replace this with your 'vcvars64' if it's different
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
 
 set output_file_name=Snake
 
-rem Replace this with your Window Kits
-set winsdk="C:/Program Files (x86)/Windows Kits/10/lib/10.0.18362.0/um/x64"
-
 start /b /wait "" "as" first.s -o build/first.obj
-start /b /wait "" "cl" build/first.obj /link /nologo /libpath:%winsdk% /machine:x64 /subsystem:windows /debug:none /entry:entry /out:%output_file_name%.exe kernel32.lib user32.lib Gdi32.lib
+start /b /wait "" "cl" build/first.obj /link /nologo /machine:x64 /subsystem:windows /debug:none /entry:entry /out:%output_file_name%.exe kernel32.lib user32.lib Gdi32.lib
